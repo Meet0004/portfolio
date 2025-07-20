3946c9acd89ea3c0388fe975847c0e661327dc6d
Meet Soni <113117130+Meet0004@users.noreply.github.com>
2025-07-20 11:24:37 +0530

    Update PortfolioHeader.jsx

--- src/components/Project/PortfolioHeader.jsx
+++ src/components/Project/PortfolioHeader.jsx
@@ -3,9 +3,9 @@ const PortfolioHeader = () => {
 		<div id='projects' className="relative text-center pt-18 pb-3 px-5 overflow-hidden h-fit">
 
 			<div className="text-4xl md:text-6xl sm:3xl font-extrabold ">
-				<span className="font-extrabold bg-gradient-to-r from-[#0243f7] to-[#dd0c0c] bg-clip-text text-transparent leading-tight">Some</span> 
+				<span className="font-extrabold bg-gradient-to-r from-white to-white bg-clip-text text-transparent leading-tight">Some</span> 
 				💡 
-				<span className="font-extrabold bg-gradient-to-r from-[#df3b09] via-red-600 to-[#e0a503] bg-clip-text text-transparent leading-tight">cooked into</span>
+				<span className="font-extrabold bg-gradient-to-r from-white via-blue-200 to-blue-400 bg-clip-text text-transparent leading-tight">cooked into</span>
 				💻
 				<span className="text-red-400 animate-pulse">....</span>
 			</div>
