afbfcbbd31eff7de74becf2d4b4278c724667c12
Meet Soni <113117130+Meet0004@users.noreply.github.com>
2025-07-20 11:26:57 +0530

    Update PortfolioHeader.jsx

--- src/components/Project/PortfolioHeader.jsx
+++ src/components/Project/PortfolioHeader.jsx
@@ -2,7 +2,7 @@ const PortfolioHeader = () => {
 	return (
 		<div id='projects' className="relative text-center pt-18 pb-3 px-5 overflow-hidden h-fit">
 
-			<div className="text-3xl md:text-6xl sm:3xl font-extrabold ">
+			<div className="text-3xl md:text-6xl sm:2xl font-extrabold ">
 				<span className="font-extrabold bg-gradient-to-r from-white to-white bg-clip-text text-transparent leading-tight">Some</span> 
 				💡 
 				<span className="font-extrabold bg-gradient-to-r from-white via-blue-200 to-blue-400 bg-clip-text text-transparent leading-tight">cooked into</span>
