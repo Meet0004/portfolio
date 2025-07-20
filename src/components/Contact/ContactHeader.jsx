a404ee7e6410a1c3339c00715ee7c0366cb67a84
Meet Soni <113117130+Meet0004@users.noreply.github.com>
2025-07-20 11:28:58 +0530

    Update ContactHeader.jsx

--- src/components/Contact/ContactHeader.jsx
+++ src/components/Contact/ContactHeader.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ContactHeader = () => (
   <div className="text-center mb-12">
-    <p className="text-5xl md:text-6xl font-bold bg-gradient-to-r from-white to-blue-400 bg-clip-text text-transparent  mb-6 ">
+    <p className="text-5xl md:text-6xl font-bold bg-gradient-to-r from-white to-white bg-clip-text text-transparent  mb-6 ">
       Contact{' '}
-      <span className=" bg-gradient-to-r from-purple-600 to-blue-400  bg-clip-text text-transparent">
+      <span className=" bg-gradient-to-r from-white to-blue-400  bg-clip-text text-transparent">
         Me
       </span>
     </p>
