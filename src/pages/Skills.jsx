1cf51d9931d5915ee53f72fd8ec21480ae94f1e4
Meet Soni <113117130+Meet0004@users.noreply.github.com>
2025-07-20 11:22:57 +0530

    Update Skills.jsx

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -17,7 +17,7 @@ const Skills = () => {
 			{/* Background Effect */}
 			<div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg%20width%3D%2260%22%20height%3D%2260%22%20viewBox%3D%220%200%2060%2060%22%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%3E%3Cg%20fill%3D%22none%22%20fill-rule%3D%22evenodd%22%3E%3Cg%20fill%3D%22%239C92AC%22%20fill-opacity%3D%220.05%22%3E%3Ccircle%20cx%3D%2230%22%20cy%3D%2230%22%20r%3D%221%22/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-20"></div>
 
-			<h2 className="text-6xl font-bold text-center mb-12 bg-gradient-to-r from-purple-800 to-blue-400 bg-clip-text text-transparent">
+			<h2 className="text-6xl font-bold text-center mb-12 bg-gradient-to-r from-white to-blue-400 bg-clip-text text-transparent">
 				Technical Skills
 			</h2>
 
