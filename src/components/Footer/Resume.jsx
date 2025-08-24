e4019ac674c9168a4fdbc18d06657bb85ee10c72
Meet Soni <113117130+Meet0004@users.noreply.github.com>
2025-08-24 23:59:38 +0530

    Update Resume.jsx

--- src/components/Footer/Resume.jsx
+++ src/components/Footer/Resume.jsx
@@ -4,8 +4,8 @@ import { FaFilePdf } from 'react-icons/fa';
 const DownloadResume = ({ className = '' }) => {
   return (
     <a
-      href=".Meet G Soni-resume.pdf"
-      download="Meet G Soni-resume.pdf"
+      href=".ms.pdf"
+      download="ms.pdf"
       className={`
         flex items-center 
         bg-blue-600 hover:bg-blue-700 
